b7f74b59b3c24add8e31fe6a0e6e28846cce7c1f
beza-hash1 <bezawitabera512@gmail.com>
2025-07-17 01:31:33 +0300

    Add 10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2]);
+
+if (isNaN(arg)) {
+  console.log(1);
+} else {
+  console.log(factorial(arg));
+}
