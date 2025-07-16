34ac80b967600f63fb17157660734594b1d64307
beza-hash1 <bezawitabera512@gmail.com>
2025-07-17 01:26:22 +0300

    Add 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,14 @@
 const arg = process.argv[2];
 const x = parseInt(arg);
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (x > 0) {
   let i = 0;
   while (i < x) {
     console.log('C is fun');
     i++;
   }
 }
+// if x is 0 or negative, print nothing (no output)
+
